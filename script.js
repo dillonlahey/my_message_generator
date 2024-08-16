2b22c0918a5e0809977fa97575cfdd9ee5f9367b
Dillon Lahey <dillonlahey@gmail.com>
2024-08-16 16:25:53 -0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hello");
+function generateRandomNumber(num) {
+    // from 0 -> 1
+    return Math.floor(Math.random() * num)
+}
+
+const manifestIdeas = {
+    trueName: ['dj provai', 'saoirse', 'moglai', 'cillian'],
+    northCounty: ['armagh', 'fermanagh', 'tyrone', 'antrim', 'derry', 'down'],
+    kneeCapSong: ['get yer brits out', 'fine art', 'c e a r t a', 'sick in the head', 'better way to live']
+}
+
+let irishManifest = []
+
+for (let item in manifestIdeas) {
+    let optionIdx = generateRandomNumber(manifestIdeas[item].length)
+
+    switch(item) {
+        case 'trueName':
+            irishManifest.push(`Your true name is ${manifestIdeas[item][optionIdx]}.`)
+            break
+        case 'northCounty':
+            irishManifest.push(`Your county in the North is ${manifestIdeas[item][optionIdx]}.`)
+            break
+        case 'kneeCapSong':
+            irishManifest.push(`Your Kneecap song of the week is ${manifestIdeas[item][optionIdx]}.`)
+            break
+        default:
+            irishManifest.push(`Insufficient information. tiocfadh ar la.`)
+    }
+}
+
+function formatManifestation(manifest) {
+    const formatted = irishManifest.join('\n')
+    console.log(formatted)
+}
+
+formatManifestation(irishManifest);
